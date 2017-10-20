511e374ecf7351b334e769e10948da30cdcb07e9
brandongossen <brandongossen@yahoo.com>
2017-10-20 11:53:22 -0500

    JDBC First Commit
    -Create the database adlister_db
    
    -Create two tables: users-migration and ads-migration
    
    -Connects foreign key (user_id) to users table

---
+++ src/main/migration_tables/ads-migration.sql
@@ -0,0 +1,18 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE ads (
+
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+  user_id INT UNSIGNED NOT NULL,
+
+  title VARCHAR (200) NOT NULL,
+
+  description TEXT NOT NULL,
+
+  PRIMARY KEY (id),
+
+  FOREIGN KEY(user_id) REFERENCES users (id)
+);
